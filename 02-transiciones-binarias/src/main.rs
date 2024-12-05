11c028b5f664e161a34ef1f9daa88cb6eab971fc
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-12-04 23:33:37 -0600

    [02] fixed: Se arregla problema de tipado para números grandes

--- 02-transiciones-binarias/src/main.rs
+++ 02-transiciones-binarias/src/main.rs
@@ -1,24 +1,21 @@
 use std::io;
+
 fn main() {
-
-    // println!("Ingresa un dato");
     let mut data = String::new();
-    io::stdin().read_line( &mut data).ok().expect("No se pudo leer el dato");
-    // println!("El dato es {}", data.trim());
+    io::stdin().read_line(&mut data).ok().expect("No se pudo leer el dato");
 
-    let n: u32 = data.trim().parse().expect("No es un número");
+    let n: u128 = data.trim().parse().expect("No es un número");
 
     println!("{}", transiciones(n));
-
 }
 
-
-fn transiciones (n: u32) -> u32 {
-
+fn transiciones(n: u128) -> u128 {
     if n < 2 {
-        return  0;
+        return 0;
     }
+    2 * transiciones(n - 1) + pow_u128(2, n - 1)
+}
 
-    return  2 * transiciones(n - 1) + 2_u32.pow(n - 1);
-    
+fn pow_u128(base: u128, exp: u128) -> u128 {
+    base.pow(exp as u32)
 }
